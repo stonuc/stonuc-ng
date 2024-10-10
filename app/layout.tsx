bb885b94ff235e20c2b8fe9fd71ce67f85fd18ea
CZEJAY <calebjimmy67@gmail.com>
2024-10-10 13:04:03 +0100

    "Added metadata configuration for SEO and social media sharing"

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/components/sections/Footer";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -17,6 +21,83 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Stonuc Technologies",
   description: "Software Consulting and Development",
+
+  applicationName:  "Stonuc Technologies",
+
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      noimageindex: true,
+    },
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    site: "@stonuctechnologies",
+    title: "Stonuc Technologies",
+    creator:  "@roxxoncodes",
+    description: "Software Consulting and Development",
+    images: [
+      {
+        url: "/images/og1.png",
+      },
+    ]
+  },
+
+  openGraph: {
+    url: "/",
+    type: "website",
+    title: "Stonuc Technologies",
+    description: "Software Consulting and Development",
+    images: [
+      {
+        url: "/images/og1.png",
+      },
+    ],
+    siteName:  "Stonuc Technologies",
+  },
+
+  authors: [
+    {
+      name: "Caleb Jimmy",
+      url: "https://github.com/CZEJAY",
+    },
+  ],
+
+  keywords: [
+    "software development",
+    "software consulting",
+    "software architecture",
+    "software design",
+    "software engineering",
+    "software testing",
+    "software quality assurance",
+    "software project management",
+    "software development methodologies",
+    "software development frameworks",
+    "software development tools",
+    "software development resources",
+    "software development best practices",
+    "software development industry",
+    "software development job opportunities",
+    "software development company",
+    "software development company recruitment",
+    "software development company hiring",
+    "software development company management",
+    "software development company culture",
+    "software development company values",
+    "software development company benefits",
+    "software development company success stories",
+    "software development company case studies",
+    "software development company market analysis",
+    "software development company market trends",
+    "software development company market share",
+    "software development company competitors",
+    "software development company market demand",
+  ]
 };
 
 export default function RootLayout({
